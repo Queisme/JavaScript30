a2ad035fdbed12fe5cc5386027a16baa03562b3a
Queisme <queiskey@gmail.com>
2024-08-06 00:05:31 -0500

    Object Basics

--- 04 - Array Cardio Day 1/script.js
+++ 04 - Array Cardio Day 1/script.js
@@ -96,13 +96,38 @@ console.table(sortByBirth);
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
 
+const totalYears = inventors.reduce((total, inventor) => {
+  return total + (inventor.passed - inventor.year);
+}, 0);
+
+console.log(totalYears);
+
 // 5. Sort the inventors by years lived
+
+const sortByYearsLived = inventors.sort((a, b) => {
+  const lastPerson = a.passed - a.year;
+  const nextPerson = b.passed - b.year;
+  return lastPerson > nextPerson ? -1 : 1;
+});
+
+console.table(sortByYearsLived);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
+// const category = document.querySelector('.mw-category');
+// const links = [...category.querySelectorAll('a')];
+
+// const de = links
+//   .map((link) => link.textContent)
+//   .filter(streetname => streetname.includes('de'));
+
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+
+const peopleNames = people.sort((a, b) => (a > b ? 1 : -1));
+
+console.table(peopleNames);
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
@@ -122,3 +147,13 @@ const data = [
   'car',
   'truck',
 ];
+
+const times = data.reduce((obj, item) => {
+  if (!obj[item]) {
+    obj[item] = 0;
+  }
+  obj[item]++;
+  return obj;
+}, {});
+
+console.log(times);
---
+++ 07 - Array Cardio Day 2/script.js
@@ -0,0 +1,54 @@
+// ## Array Cardio Day 2
+
+const people = [
+  { name: 'Wes', year: 1988 },
+  { name: 'Kait', year: 1986 },
+  { name: 'Irv', year: 1970 },
+  { name: 'Lux', year: 2015 },
+];
+
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen is my fav food ever', id: 123523 },
+  { text: 'Nice Nice Nice!', id: 542328 },
+];
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+
+const atLeast = people.some((person) => person.year <= 2005);
+
+console.log(atLeast);
+
+// Array.prototype.every() // is everyone 19 or older?
+
+const everyOne = people.every((peeps) => peeps.year <= 2005);
+
+console.log(everyOne);
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+
+const findComm = comments.find((comment) => comment.id === 823423);
+
+console.log(findComm);
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+
+// const index = comments.findIndex((comment) => comment.id === 823423);
+
+// console.log(index); // 1
+
+// to delete
+// comments.splice(index, 1);
+
+// console.table(comments);
+
+// const newComments = [...comments.slice(0, index), ...comments.slice(index + 1)];
+
+console.table(newComments);
